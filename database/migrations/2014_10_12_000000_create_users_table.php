6751b4d2e3b14c468db3abab3122eb812ea47f52
vattgern <sashawot060@gmail.com>
2023-02-27 16:17:41 +0400

    Страница сниппетов начало положено

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('first_name',255);
+            $table->string('last_name',255);
+            $table->string('email',255)->unique();
+            $table->string('additional_email',190);
+            $table->string('ip',255);
+            $table->string('avatar',255);
+            $table->boolean('is_verified');
+            $table->string('password_hash',100);
+            $table->string('restore_password_key', 64);
+            $table->string('auth_key',32);
+            $table->string('public_key',255);
+            $table->string('secret_key',255);
+            $table->integer('status');
+
             $table->rememberToken();
             $table->timestamps();
         });
